e1c82d949824d21727394fb59cc673c0a19b8688
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2021-12-09 23:08:29 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
